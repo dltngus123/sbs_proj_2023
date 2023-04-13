7dfe182b5ca7182058b88b3026019b3fc7e68ef5
dltngus123 <awsdr123@gmail.com>
2023-04-13 15:46:58 +0900

    호출 시 반환되는 값이 1씩 커지는 컨트롤러 구현

--- src/main/java/com/lsh/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/lsh/exam/demo/controller/UsrHomeController.java
@@ -6,10 +6,47 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
+	
+	private int count;
+	
+	public UsrHomeController() {
+		count =-1;
+	}
+	
 	@RequestMapping("/usr/home/main")
 	@ResponseBody
 	public String showMain() {
 		return "안녕하세요.";
 	}
+	
+	@RequestMapping("/usr/home/main2")
+	@ResponseBody
+	public String showMain2() {
+		return "반갑습니다.";
+		
+	}
+	
+	@RequestMapping("/usr/home/main3")
+	@ResponseBody
+	public String showMain3() {
+		return "또 만나요.";
+		
+	}
+	
+	
+	   @RequestMapping("/usr/home/getCount")
+	   @ResponseBody
+	   public int getCount() {
+	   
+	      return count;
 
+	   }
+	   
+	   @RequestMapping("/usr/home/doSetCount")
+	   @ResponseBody
+	   public String doSetCount(int count ) {
+	      this.count = count;
+	      return "count의 값이 " + this.count + "(으)로 초기화";
+
+	   }
 }
