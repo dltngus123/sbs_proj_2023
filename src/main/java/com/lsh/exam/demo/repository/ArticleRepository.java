c3956ccce9fcfc8bbd7bb25084a4fde0b3acac07
dltngus123 <awsdr123@gmail.com>
2023-04-14 18:18:54 +0900

    INSERT 쿼리를 메서드와 연결, last_insert_id 사용

--- src/main/java/com/lsh/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/lsh/exam/demo/repository/ArticleRepository.java
@@ -3,6 +3,7 @@ package com.lsh.exam.demo.repository;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -20,14 +21,17 @@ public interface ArticleRepository {
 	@Select("SELECT * FROM article ORDER BY id DESC")
 	public List<Article> getArticles();
 	
-	
-	public Article writeArticle(String title, String body);
+	@Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = #{title}, `body`= #{body}")
+	public void writeArticle(@Param("title") String title, @Param("body") String body);
 	
 	// delete from article where id = ?
 	@Delete("DELETE FROM article WHERE id = #{id}")
 	public void deleteArticle(@Param("id") int id);
 	
 	// update from article set title = ?, `body` = ?, updateDate = now() where id = ?
-	@Update("UPDATE article SET title = #{title}, `body` = #{body}, updateDate = NOW WHERE id = #{id}")
+	@Update("UPDATE article SET title = #{title}, `body` = #{body}, updateDate = NOW() WHERE id = #{id}")
 	public void modifyArticle(@Param("id") int id, @Param("title") String title, @Param("body") String body);
+
+	@Select("SELECT LAST_INSERT_ID()")
+	public int getLastInsertId();
 }
