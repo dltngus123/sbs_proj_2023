808fe28d4f0ce6b4a6ea260190a929f8d3c52baf
dltngus123 <awsdr123@gmail.com>
2023-04-14 18:46:24 +0900

    SQL을 xml 파일로 옮김

--- src/main/java/com/lsh/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/lsh/exam/demo/repository/ArticleRepository.java
@@ -2,12 +2,8 @@ package com.lsh.exam.demo.repository;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 
 import com.lsh.exam.demo.vo.Article;
 
@@ -15,23 +11,23 @@ import com.lsh.exam.demo.vo.Article;
 public interface ArticleRepository {
 	
 	// select * from article where id = ?
-	@Select("SELECT * FROM article WHERE id = #{id}")
+	//@Select("SELECT * FROM article WHERE id = #{id}")
 	public Article getArticle(@Param("id") int id);
 
-	@Select("SELECT * FROM article ORDER BY id DESC")
+	//@Select("SELECT * FROM article ORDER BY id DESC")
 	public List<Article> getArticles();
 	
-	@Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = #{title}, `body`= #{body}")
+	//@Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = #{title}, `body`= #{body}")
 	public void writeArticle(@Param("title") String title, @Param("body") String body);
 	
 	// delete from article where id = ?
-	@Delete("DELETE FROM article WHERE id = #{id}")
+	//@Delete("DELETE FROM article WHERE id = #{id}")
 	public void deleteArticle(@Param("id") int id);
 	
 	// update from article set title = ?, `body` = ?, updateDate = now() where id = ?
-	@Update("UPDATE article SET title = #{title}, `body` = #{body}, updateDate = NOW() WHERE id = #{id}")
+	//@Update("UPDATE article SET title = #{title}, `body` = #{body}, updateDate = NOW() WHERE id = #{id}")
 	public void modifyArticle(@Param("id") int id, @Param("title") String title, @Param("body") String body);
 
-	@Select("SELECT LAST_INSERT_ID()")
+	//@Select("SELECT LAST_INSERT_ID()")
 	public int getLastInsertId();
 }
