27f00b67c32ce4fc1254465ec1f9cbd15d1ede3c
dltngus123 <awsdr123@gmail.com>
2023-04-17 16:08:17 +0900

    MemberRepository 추가후, 회원가입 처리

--- src/main/java/com/lsh/exam/demo/service/MemberService.java
+++ src/main/java/com/lsh/exam/demo/service/MemberService.java
@@ -2,12 +2,18 @@ package com.lsh.exam.demo.service;
 
 import org.springframework.stereotype.Service;
 
+import com.lsh.exam.demo.repository.MemberRepository;
+
 @Service
 public class MemberService {
 	private MemberRepository memberRepository;
+	
+	public MemberService(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
 
 	public void join(String loginId, String loginPw, String name, String nickname, String email, String cellphoneNo) {
-		// TODO Auto-generated method stub
+		memberRepository.join( loginId, loginPw, name, nickname,email, cellphoneNo);
 		
 	}
 	
