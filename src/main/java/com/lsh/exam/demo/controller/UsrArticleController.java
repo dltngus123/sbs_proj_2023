c2c8453968c6c8bc4c635b97b69a8eecda3581c0
dltngus123 <awsdr123@gmail.com>
2023-04-18 17:50:27 +0900

    게시물 작성시, 로그인 여부 체크, 작성자 정보 저장

--- src/main/java/com/lsh/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/lsh/exam/demo/controller/UsrArticleController.java
@@ -12,6 +12,8 @@ import com.lsh.exam.demo.utill.Ut;
 import com.lsh.exam.demo.vo.Article;
 import com.lsh.exam.demo.vo.ResultData;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class UsrArticleController {
 	@Autowired
@@ -20,7 +22,21 @@ public class UsrArticleController {
 	// 액션 메서드 시작
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
-	public ResultData<Article> doAdd(String title, String body) {
+	public ResultData<Article> doAdd(HttpSession httpSession, String title, String body) {
+		
+		boolean isLogined = false;
+		
+		int loginedMemberId = 0;
+		
+		if (httpSession.getAttribute("loginedMemberId") != null) {
+			
+			isLogined = true;
+			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
+		}
+		
+		if ( isLogined == false) {
+			return ResultData.from("F-A","로그인 후 이용 해주세요.");
+		}
 		
 		//만약 title or body 입력을 안했을때
 		if(Ut.empty(title)) {
@@ -31,7 +47,7 @@ public class UsrArticleController {
 			return ResultData.from("F-2", "body을(를) 입력 해주세요.");
 		}
 		
-		ResultData<Integer> writeArticleRd = articleService.writeArticle(title, body);
+		ResultData<Integer> writeArticleRd = articleService.writeArticle(loginedMemberId, title, body);
 		
 		int id = writeArticleRd.getData1();
 		
--- src/main/java/com/lsh/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/lsh/exam/demo/repository/ArticleRepository.java
@@ -18,7 +18,7 @@ public interface ArticleRepository {
 	public List<Article> getArticles();
 	
 	//@Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = #{title}, `body`= #{body}")
-	public void writeArticle(@Param("title") String title, @Param("body") String body);
+	public void writeArticle(@Param("memberId") int memberId, @Param("title") String title, @Param("body") String body);
 	
 	// delete from article where id = ?
 	//@Delete("DELETE FROM article WHERE id = #{id}")
