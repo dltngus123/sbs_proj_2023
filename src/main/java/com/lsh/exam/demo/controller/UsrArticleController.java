6a4c0e6ff61878799b3a3d5779f3ec5850b0d894
dltngus123 <awsdr123@gmail.com>
2023-04-24 16:40:50 +0900

    게시물 리스트 page 파라미터 처리

--- src/main/java/com/lsh/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/lsh/exam/demo/controller/UsrArticleController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.lsh.exam.demo.service.ArticleService;
@@ -14,8 +15,6 @@ import com.lsh.exam.demo.vo.Article;
 import com.lsh.exam.demo.vo.Board;
 import com.lsh.exam.demo.vo.ResultData;
 import com.lsh.exam.demo.vo.Rq;
-
-import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class UsrArticleController {
@@ -60,7 +59,7 @@ public class UsrArticleController {
 	}
 	
 	@RequestMapping("/usr/article/list")
-	public String showList( Model model, int boardId) {
+	public String showList( Model model, @RequestParam(defaultValue = "1") int boardId, @RequestParam(defaultValue = "1") int page) {
 		
 		Board board = boardService.getBoardById(boardId);
 		if (board == null) {
@@ -68,8 +67,10 @@ public class UsrArticleController {
 		}
 		
 		int articlesCount = articleService.getArticlesCount(boardId);
+		//페이지네이션 1페이지에 10개씩 보여준다
+		int itemsCountInAPage = 10;
 		
-		List<Article> articles = articleService.getForPrintArticles(rq.getLoginedMemberId(), boardId);
+		List<Article> articles = articleService.getForPrintArticles(rq.getLoginedMemberId(), boardId, itemsCountInAPage, page);
 		
 		model.addAttribute("board", board);
 		model.addAttribute("articlesCount", articlesCount);
