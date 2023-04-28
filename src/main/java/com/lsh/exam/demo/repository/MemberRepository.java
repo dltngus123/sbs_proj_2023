2eeda6488e7ce7652a88566cdf79db2fd35dba4f
dltngus123 <awsdr123@gmail.com>
2023-04-28 17:44:21 +0900

    회원정보 수정처리

--- src/main/java/com/lsh/exam/demo/repository/MemberRepository.java
+++ src/main/java/com/lsh/exam/demo/repository/MemberRepository.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.lsh.exam.demo.vo.Member;
 
@@ -59,6 +60,33 @@ public interface MemberRepository {
 			OR M.email = #{email}
 			""")
 	public Member getMemberByNameAndEmail(@Param("name")String name,@Param("email") String email);
+	
+	@Update("""
+			<script>
+			UPDATE `member`
+			<set>
+				updateDate = NOW(),
+				<if test="loginPw != null">
+					loginPw = #{loginPw},
+				</if>
+				<if test="name != null">
+					name = #{name},
+				</if>
+				<if test="nickname != null">
+					nickname = #{nickname},
+				</if>
+				<if test="email != null">
+					email = #{email},
+				</if>
+				<if test="cellphoneNo != null">
+					cellphoneNo = #{cellphoneNo},
+				</if>
+			</set>
+				WHERE id = #{id}
+			</script>
+			""")
+	public void modify(int id, String loginPw, String name, String nickname, String email,
+			String cellphoneNo);
 	
 	
 
