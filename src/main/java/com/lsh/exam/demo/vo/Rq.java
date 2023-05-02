7fdf791a99663c2f27ba41b5f0cd06e6c5d3071d
dltngus123 <awsdr123@gmail.com>
2023-05-02 15:47:26 +0900

    로그아웃 상태에서 작성 버튼 누르면, 로그인필요 메세지와 함께 로그인 화면으로 이동

--- src/main/java/com/lsh/exam/demo/vo/Rq.java
+++ src/main/java/com/lsh/exam/demo/vo/Rq.java
@@ -122,6 +122,11 @@ public class Rq {
 	//Rq 객체가 자연스럽게 생성되도록 유도하는 역할
 	//지우면 안됨,편의를 위해 BeforeActionInterceptor에서 꼭 호출 해줘야함.
 	public void initOnBeforeActionInterceptor() {}
+
+	public void printReplaceJs(String msg, String uri) {
+		resp.setContentType("text/html; charset=UTF-8");
+		print(Ut.jsReplace(msg, uri));
+	}
 	
 	
 }
